4d677e247dfcfd57f9b2fe7f568098923091a5e3
drkitty <bumblingkitty@gmail.com>
2014-03-25 02:50:19 -0700

    Change more spaces to tabs

--- io.c
+++ io.c
@@ -3,41 +3,41 @@
 
 void sigint_handler(int signum)
 {
-    fputs("\33[?1049l", stdout);
-    exit(0);
+	fputs("\33[?1049l", stdout);
+	exit(0);
 }
 
 
 void setup_signal_handlers()
 {
-    struct sigaction sa;
-    sa.sa_handler = sigint_handler;
+	struct sigaction sa;
+	sa.sa_handler = sigint_handler;
 
-    sigset_t mask;
-    sigemptyset(&mask);
-    sa.sa_mask = mask;
+	sigset_t mask;
+	sigemptyset(&mask);
+	sa.sa_mask = mask;
 
-    sa.sa_flags = 0;
+	sa.sa_flags = 0;
 
-    sigaction(SIGINT, &sa, NULL);
+	sigaction(SIGINT, &sa, NULL);
 }
 
 
 void initialize_tty(struct termios* term_settings) {
-    setup_signal_handlers();
-    fputs("\33[?1049h", stdout);
+	setup_signal_handlers();
+	fputs("\33[?1049h", stdout);
 
-    if (!isatty(STDOUT_FILENO)) {
-        fputs("Output is not a tty. Dying.\n", stderr);
-        exit(1);
-    }
+	if (!isatty(STDOUT_FILENO)) {
+		fputs("Output is not a tty. Dying.\n", stderr);
+		exit(1);
+	}
 
-    tcgetattr(STDOUT_FILENO, term_settings);
+	tcgetattr(STDOUT_FILENO, term_settings);
 
-    term_settings->c_lflag &= ~(ICANON | ECHO);
+	term_settings->c_lflag &= ~(ICANON | ECHO);
 
-    tcsetattr(STDOUT_FILENO, TCSADRAIN, term_settings);
+	tcsetattr(STDOUT_FILENO, TCSADRAIN, term_settings);
 
-    setvbuf(stdin, NULL, _IONBF, BUFSIZ);
-    setvbuf(stdout, NULL, _IONBF, BUFSIZ);
+	setvbuf(stdin, NULL, _IONBF, BUFSIZ);
+	setvbuf(stdout, NULL, _IONBF, BUFSIZ);
 }
