3cac45cff640272e51289e0e4381e7ace1bed1f1
Wade Cline <wadecline@hotmail.com>
2015-04-30 22:38:16 -0700

    Add explicit GPL header to files.

--- board.h
+++ board.h
@@ -1,18 +1,18 @@
 /*
- *  Copyright 2014 by Wade T. Cline
+ *  Copyright 2014-2015 by Wade T. Cline (Frostsnow)
  *
- *  This program is free software: you can redistribute it and/or modify
+ *  "2048" is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
 
- *  This program is distributed in the hope that it will be useful,
+ *  "2048" is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU General Public License for more details.
 
  *  You should have received a copy of the GNU General Public License
- *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with "2048".  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef board_H
--- save.h
+++
@@ -1,7 +0,0 @@
-#ifndef save_H
-#define save_H
-
-
-save_score(
-
-#endif // save_H
